e4222cc3bac586fb747890bb2b51c7fc6a02e7f8
Guillaume Vincent <gvincent@oslab.fr>
2013-07-23 12:39:01 +0200

    fix logging problem

--- settings.py
+++ settings.py
@@ -4,13 +4,13 @@ import base64
 import logging.config
 from config.config import Config
 
-APPLICATION_NAME = '$your_application_name'
+APPLICATION_NAME = '$YOUR_APPLICATION_NAME'
 
 PROJECT_DIR = os.path.dirname(__file__)
 TEMPLATE_DIR = os.path.join(PROJECT_DIR, APPLICATION_NAME, 'templates')
 STATIC_DIR = os.path.join(PROJECT_DIR, 'static')
-CONFIG_FILE = os.path.join(PROJECT_DIR, 'config', 'config.ini')
 
+CONFIG_FILE = os.path.join(PROJECT_DIR, 'config', 'config.ini')
 config = Config()
 config.read(CONFIG_FILE)
 
@@ -28,5 +28,55 @@ else:
     with open(CONFIG_FILE, 'wt') as cf:
         config.write(cf)
 
-LOGGING_FILE = os.path.join(PROJECT_DIR, 'logs', 'logging.json')
-logging.config.dictConfig(LOGGING_FILE)
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(module)s - %(process)d - %(thread)d - %(message)s',
+            'datefmt': '%d/%m/%Y %Hh%Mm%Ss'
+        },
+        'simple': {
+            'format': '%(asctime)s - %(name)-12s - %(levelname)-8s - %(message)s',
+            'datefmt': '%d/%m/%Y %Hh%Mm%Ss'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'stream': 'ext://sys.stdout'
+        },
+        'info_file_handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'simple',
+            'filename': 'logs/info.log',
+            'maxBytes': '10485760',
+            'backupCount': '5',
+            'encoding': 'utf8'
+        },
+        'error_file_handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'verbose',
+            'filename': 'logs/errors.log',
+            'maxBytes': '10485760',
+            'backupCount': '5',
+            'encoding': 'utf8'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'info_file_handler', 'error_file_handler'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+}
+
+LOGGING_DIR = os.path.join(PROJECT_DIR, 'logs')
+if not os.path.isdir(LOGGING_DIR):
+    os.makedirs(LOGGING_DIR)
+logging.config.dictConfig(LOGGING_CONFIG)
