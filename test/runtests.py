d14b2c40e22bf0b739890346456e7eac658e6099
Guillaume Vincent <gvincent@oslab.fr>
2013-07-23 13:35:55 +0200

    pep8 tests files improvement

--- test/runtests.py
+++ test/runtests.py
@@ -4,7 +4,7 @@ import logging
 
 logging.disable(logging.CRITICAL)
 
-test_file_paths = glob.glob('Test*.py')
+test_file_paths = glob.glob('test_*.py')
 test_names = [path[:-3] for path in test_file_paths]
 test_suites = [unittest.defaultTestLoader.loadTestsFromName(name) for name in test_names]
 suite = unittest.TestSuite(test_suites)
